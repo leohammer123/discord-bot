bdc578784abe075a34d722dc3714eeb8cb6d0d5f
loas <66953227+leohammer123@users.noreply.github.com>
2022-01-23 21:00:22 +0800

    delete

--- challenge/src/6.py
+++
@@ -1,9 +0,0 @@
-import requests
-import requests.exceptions
-flag = "flag{this_is_a_fake_flag}"
-url = input()
-
-res = requests.get(url)
-if res.status_code==200:
-    if res.text.find("<h1>Hello world</h1>")!=-1:
-        requests.get(url,params={"flag":flag})       
